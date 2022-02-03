56947473b2e2c1c7a3f147f3f7619a5be9fd97cd
dreamer20 <gv4alex@gmail.com>
2022-02-03 04:30:18 +0300

    RadioButton refactoring

--- src/App.js
+++ src/App.js
@@ -1,172 +1,44 @@
 import React, { useRef, useState } from 'react';
 import './App.scss';
-import Menu from './Menu';
-import MenuBar from './MenuBar';
-import MenuItem from './MenuItem';
-import ImagePreview from './ImagePreview';
-import Modal from './Modal';
-import RotateForm from './RotateForm';
+
+import RadioButton from './RadioButton';
 
 const api_url = 'http://localhost:5000/api/';
 
 function App() {
+  const [degree, setDegree] = useState(0);
+  const [checkedRadio, setCheckedRadio] = useState(0);
 
-  const [openedImgSrc, setOpenedImgSrc] = useState(null);
-  const [openedModal, setOpenedModal] = useState(null);
-  const [resultImgSrc, setResultImgSrc] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
-  const imgInputFileEl = useRef(null);
-
-  function processImage(tool_name, processOptions) {
-    if (!imgInputFileEl.current.files[0]) {
-      showErrorMessage('Файл не выбран.');
-      return;
-    }
-    const formData = new FormData();
-    formData.append(
-      'image',
-      imgInputFileEl.current.files[0],
-      imgInputFileEl.current.files[0].name
-    );
-    for (let key in processOptions) {
-      formData.append(key, processOptions[key])
-    }
-    const fetchOptions = {
-      method: 'POST',
-      body: formData,
-      mode: 'cors'
-    };
-
-    fetch(api_url + tool_name, fetchOptions)
-      .then((response) => {
-        if (response.status === 400) {
-          response.json().then(data => showErrorMessage(data.error));
-        } else if (response.status === 200) {
-          response.blob().then(blob => {
-          const src = URL.createObjectURL(blob);
-          setResultImgSrc(src);
-          });
-        } else {
-          alert('Непредвиденная ошибка');
-        }
-      });
-
-    }
-    // check if file is available else show message
-    // create form
-    // send data and get response
-    // if response status is 400 then show error message
-    // if response status not 200 and 400 then show enother error message
-    // if response is ok then get blob data
-    // create url for image and update state
-
-  function openImage() {
-    imgInputFileEl.current.click();
-  }
-
-  function handleImgInputFileChange(e) {
-    const imgUrl = URL.createObjectURL(e.target.files[0]);
-    setOpenedImgSrc(imgUrl);
-  }
-
-  function reflectImage(direction) {
-    processImage('reflect', {direction: direction});
-  }
-
-  function rotateImage(degree) {
-    if (degree) {
-      processImage('rotate', {degree: degree});
-    } else {
-      setOpenedModal('rotate');
-    }
-  }
-
-  function applyFilter(filterName) {
-    processImage('filter', {filterName});
-  }
-
-  function showErrorMessage(message) {
-    setErrorMessage(message);
-    setOpenedModal('error');
-  }
-
-  function closeModal() {
-    setOpenedModal(null)
+  function handleChange(value) {
+    setDegree(value);
+    setCheckedRadio(value);
   }
 
   return (
     <div className="App">
-      <header className="App__header">
-        <MenuBar>
-          <Menu title="Изображение">
-            <MenuItem onClick={openImage}>
-              Открыть
-            </MenuItem>            
-          </Menu>
-          <Menu title="Отражение">
-            <MenuItem onClick={() => reflectImage('horizontally')}>
-              Отразить по горизонтали
-            </MenuItem>            
-            <MenuItem onClick={() => reflectImage('vertically')}>
-              Отразить по вертикали
-            </MenuItem>            
-          </Menu>
-          <Menu title="Вращение">
-            <MenuItem border onClick={() => rotateImage()}>
-              Повернуть изображение
-            </MenuItem>            
-            <MenuItem onClick={() => rotateImage('90')}>
-              Повернуть на 90&#176;
-            </MenuItem>            
-            <MenuItem onClick={() => rotateImage('180')}>
-              Повернуть на 180&#176;
-            </MenuItem>            
-            <MenuItem onClick={() => rotateImage('270')}>
-              Повернуть на 270&#176;
-            </MenuItem>            
-          </Menu>
-          <Menu title="Фильтры">
-            <MenuItem onClick={() => applyFilter('CONTOUR')}>
-              Контур
-            </MenuItem>                        
-            <MenuItem onClick={() => applyFilter('DETAIL')}>
-              Уточнение
-            </MenuItem>                        
-            <MenuItem onClick={() => applyFilter('EMBOSS')}>
-              Тиснение
-            </MenuItem>                        
-            <MenuItem onClick={() => applyFilter('EDGE_ENHANCE')}>
-              Уточнить край
-            </MenuItem>                        
-            <MenuItem onClick={() => applyFilter('SMOOTH')}>
-              Сгладить
-            </MenuItem>                        
-            <MenuItem onClick={() => applyFilter('FIND_EDGES')}>
-              Выделение краев
-            </MenuItem>                        
-          </Menu>
-        </MenuBar>
-      </header>
-      <h2 className="App__title">Оригинал:</h2>
-      <ImagePreview onClick={openImage} src={openedImgSrc} image='original' />
-      <h2 className="App__title">Результат:</h2>
-      <ImagePreview image='result' src={resultImgSrc} />
-      <input
-        ref={imgInputFileEl} 
-        type="file"
-        accept="image/*" 
-        onChange={handleImgInputFileChange}
-        className="App__imgInput" /> 
-      <RotateForm
-          onApply={rotateImage}
-          onClose={closeModal}
-          isVisible={openedModal === 'rotate' ? true : false}/>
-      <Modal
-        onClose={closeModal}
-        isVisible={openedModal === 'error' ? true : false}
-        onApply={closeModal}>
-        {errorMessage}
-      </Modal>
+      <RadioButton
+        text="90"
+        name="test"
+        checked={checkedRadio === 90 ? true : false} 
+        onChange={() => handleChange(90)}/> <br />
+
+      <RadioButton
+        text="180"
+        name="test"
+        checked={checkedRadio === 180 ? true : false}
+        onChange={() => handleChange(180)}/> <br />
+
+      <RadioButton
+        text="270"
+        name="test"
+        checked={checkedRadio === 270 ? true : false}
+        onChange={() => handleChange(270)}/> <br />
+
+      <RadioButton
+        text="Другое"
+        name="test"
+        checked={checkedRadio === -1 ? true : false}
+        onChange={() => setCheckedRadio(-1)}/>
     </div>
   );
 }
--- src/RadioButton.js
+++ src/RadioButton.js
@@ -2,20 +2,20 @@ import React, { useRef, useState } from 'react';
 import './RadioButton.scss';
 
 function RadioButton (props) {
-    const activeClass = props.checked ? "RadioButton__radio-circle_checked" : ""
+
+    const checkedClass = props.checked ? 'RadioButton__checked' : "";
+
     return (
-        <label className="RadioButton">
-            <div className={`RadioButton__radio-circle ${activeClass}`}>
+        <label className={`RadioButton ${checkedClass}`}>
+            <div className="RadioButton__view">
                 <input
-                    {...props}
-                    className="RadioButton__input"
-                    type="radio"
                     name={props.name}
                     onChange={props.onChange}
-                    value={props.value} />
-                
+                    className="RadioButton__input"
+                    type="radio" />                
             </div>
-            <div className="RadioButton__text">
+            <div 
+                className="RadioButton__text">
                 {props.text}
             </div>
         </label>
